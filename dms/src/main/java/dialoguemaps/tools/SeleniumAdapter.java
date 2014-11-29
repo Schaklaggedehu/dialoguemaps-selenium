335eb68b0d174fa2f92687cf2517b4035054a844
Maddini <Maddini@192.168.13.2>
2014-11-29 14:14:29 +0100

    Update Firefox Windows path

--- dms/src/main/java/dialoguemaps/tools/SeleniumAdapter.java
+++ dms/src/main/java/dialoguemaps/tools/SeleniumAdapter.java
@@ -23,70 +23,70 @@ import org.openqa.selenium.firefox.FirefoxProfile;
  */
 public class SeleniumAdapter {
 
-	Point point = new Point(0, 0);
-	private List<WebDriver> _drivers = new ArrayList<>();
+    Point point = new Point(0, 0);
+    private List<WebDriver> _drivers = new ArrayList<>();
 
-	public List<WebDriver> getDriver() {
-		return _drivers;
-	}
+    public List<WebDriver> getDriver() {
+        return _drivers;
+    }
 
-	void initSelenium() {
-		Properties prop = PropertyReader.load();
-		FirefoxProfile firefoxProfile = new FirefoxProfile();
-		firefoxProfile.setAcceptUntrustedCertificates(true);
-		firefoxProfile.setEnableNativeEvents(true);
-		if (Boolean.parseBoolean((String) prop
-				.get("enableGWTBrowserPlugin"))) {
-			File gwtDevModeExtension = new File(
-					"browserPlugins/gwt-dev-plugin-1.26-rc1.xpi");
-			try {
-				firefoxProfile.addExtension(gwtDevModeExtension);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		if (Boolean.parseBoolean((String) prop
-				.get("enableFirebugAndFirePathBrowserPlugin"))) {
-			File firebugExtension = new File("browserPlugins/firebug-1.12.8-fx.xpi");
-			File firepathExtension = new File(
-					"browserPlugins/firepath-11900-latest.xpi");
-			try {
-				firefoxProfile.addExtension(firebugExtension);
-				firefoxProfile.addExtension(firepathExtension);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			firefoxProfile.setPreference("extensions.firebug.currentVersion",
-					"1.12.8"); // Avoid firebug extension tab
-		}
-		String firefoxPath = prop.getProperty("firefoxPath");
-		File file = new File(prop.getProperty(firefoxPath));
-		FirefoxBinary bin = new FirefoxBinary(file);
-		WebDriver driver = new FirefoxDriver(bin, firefoxProfile);
-		driver.manage().window().setPosition(point);
-		point.x = point.getX()+30;
-		point.y = point.getY()+30;
-		driver.manage().window().setSize(new Dimension(1024, 768));
+    void initSelenium() {
+        Properties prop = PropertyReader.load();
+        FirefoxProfile firefoxProfile = new FirefoxProfile();
+        firefoxProfile.setAcceptUntrustedCertificates(true);
+        firefoxProfile.setEnableNativeEvents(true);
+        if (Boolean.parseBoolean((String) prop
+                .get("enableGWTBrowserPlugin"))) {
+            File gwtDevModeExtension = new File(
+                    "browserPlugins/gwt-dev-plugin-1.26-rc1.xpi");
+            try {
+                firefoxProfile.addExtension(gwtDevModeExtension);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (Boolean.parseBoolean((String) prop
+                .get("enableFirebugAndFirePathBrowserPlugin"))) {
+            File firebugExtension = new File("browserPlugins/firebug-1.12.8-fx.xpi");
+            File firepathExtension = new File(
+                    "browserPlugins/firepath-11900-latest.xpi");
+            try {
+                firefoxProfile.addExtension(firebugExtension);
+                firefoxProfile.addExtension(firepathExtension);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            firefoxProfile.setPreference("extensions.firebug.currentVersion",
+                    "1.12.8"); // Avoid firebug extension tab
+        }
+        String firefoxPath = prop.getProperty("firefoxPath");
+        File file = new File(firefoxPath);
+        FirefoxBinary bin = new FirefoxBinary(file);
+        WebDriver driver = new FirefoxDriver(bin, firefoxProfile);
+        driver.manage().window().setPosition(point);
+        point.x = point.getX()+30;
+        point.y = point.getY()+30;
+        driver.manage().window().setSize(new Dimension(1024, 768));
 
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.manage().timeouts().pageLoadTimeout(100, TimeUnit.SECONDS);
-		driver.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
-		_drivers.add(driver);
-	}
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(100, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
+        _drivers.add(driver);
+    }
 
-	public void quit() {
-		for (WebDriver driver : _drivers) {
-			driver.quit();			
-		}
-	}
+    public void quit() {
+        for (WebDriver driver : _drivers) {
+            driver.quit();          
+        }
+    }
 
-	public boolean isElementPresent(final WebElement element) {
-		try {
-			element.isDisplayed();
-		} catch (NoSuchElementException ex) {
-			return false;
-		}
-		return true;
-	}
+    public boolean isElementPresent(final WebElement element) {
+        try {
+            element.isDisplayed();
+        } catch (NoSuchElementException ex) {
+            return false;
+        }
+        return true;
+    }
 
 }
